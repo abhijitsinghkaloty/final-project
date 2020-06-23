642224bba0a0bf4b18ddf25368b5e4b4ab1cd7e5
Ojou Nii <jiankarlo99@gmail.com>
2020-06-23 21:05:32 +0800

    Finished badWords.py using RegEx

--- badWords.py
+++ badWords.py
@@ -1,5 +1,6 @@
 import re
 
+
 def badWordChecker(message):
     """
     Takes message as input and checks for words of profanity 
@@ -12,18 +13,38 @@ def badWordChecker(message):
     # separate all words into a list
     message = message.split(" ")
 
+    # List of profane words in the message
     profaneWords = []
+    
+    # Open data file of bad-words
     with open("./static/bad-words.txt") as data:
+        # read in the data
         badWords = data.read()
-
+        # split the words by '\n' and store them as a list
         badWords = badWords.split("\n")
 
-        for word in message:
-            if word in badWords:
-                profaneWords.append(word)
+        # Use regex on badWords
+        for word in range(len(badWords)):
+            badWords[word] = ".?" + re.escape(badWords[word]) + ".?"
 
+
+        # for every word in message
+        for word in message:
+            
+            for badWord in badWords:
+                # Check if the word is in badWord 
+                # Or if it is like the badWord
+                match = re.findall(badWord, word)
+                # if match then append word to profaneWords
+                if match:
+                    profaneWords.append(word)
+                    break
+
+
+    # if the profaneWords is empty return false
     if not profaneWords:
         return False
+    # else return the list of profane words present in the message
     else:
         return profaneWords
 
