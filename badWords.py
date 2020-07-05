1403be1f545898fe5a978f1e115ad9afb2f325da
Ojou Nii <jiankarlo99@gmail.com>
2020-07-05 23:19:15 +0800

    Revised badWords.py

--- badWords.py
+++ badWords.py
@@ -1,50 +1,26 @@
-import re
+from better_profanity import profanity as pft
 
 
 def badWordChecker(message):
     """
     Takes message as input and checks for words of profanity 
     :PARAMS: STR
-    :RETURN: FALSE / :LIST: str
+    :RETURN: BOOL
     """
     
-    # Lowercase the whole message
-    message = message.lower()
     # separate all words into a list
     message = message.split(" ")
 
-    # List of profane words in the message
-    profaneWords = []
+    for word in range(len(message)):
+
+        if message[word - 1] == "not" and word != 0:
+            print("here")
+            continue
     
-    # Open data file of bad-words
-    with open("./database/bad-words.txt") as data:
-        # read in the data
-        badWords = data.read()
-        # split the words by '\n' and store them as a list
-        badWords = badWords.split("\n")
+        if pft.contains_profanity(message[word]):
 
-        # Use regex on badWords
-        for word in range(len(badWords)):
-            badWords[word] = "^." + re.escape(badWords[word]) + ".$"
+            return True
 
+    return False
 
-        # for every word in message
-        for word in message:
-            
-            for badWord in badWords:
-                # Check if the word is in badWord 
-                # Or if it is like the badWord
-                match = re.findall(badWord, word)
-                # if match then append word to profaneWords
-                if match:
-                    profaneWords.append(word)
-                    break
-
-
-    # if the profaneWords is empty return false
-    if not profaneWords:
-        return False
-    # else return the list of profane words present in the message
-    else:
-        return profaneWords
 
